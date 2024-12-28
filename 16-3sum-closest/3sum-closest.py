f7f32053730d75b69dad782e8c52645dc6361d91
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-12-28 22:43:39 +0500

    Time: 383 ms (24.22%) | Memory: 18 MB (7.22%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        nums.sort()
+        closest = float("inf")
+        for i in range(n):  # fix one no
+            l = i + 1 # two pointer approach
+            r = n - 1
+            while l < r:
+                curr_sum = nums[i] + nums[l] + nums[r]
+                if abs(target - curr_sum) < abs(target - closest):
+                    closest = curr_sum
+
+                if curr_sum < target:
+                    l += 1
+                else:
+                    r -= 1
+        return closest
+
+        # n = len(nums)
+        # closest = float('inf') # very large +ve no
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         for k in range(j+1,n):
+        #             curr_sum = nums[i] + nums[j] + nums[k]
+
+        #             if abs(target - curr_sum) < abs(target - closest):
+        #                 closest = curr_sum
+        # return closest    # this approach gives you TLE
