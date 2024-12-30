899ca7fa013c61f08c8ed0409a0a739ba33fe60e
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2024-12-31 01:47:12 +0500

    Time: 4 ms (62.61%) | Memory: 18.6 MB (5.15%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
@@ -0,0 +1,27 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        # isk liye mje phele ye tu pta ho k har diagonal k elements konse hain, ek particular diagonal k sary elements ki i-j ki value same hoti hai this is the catch
+        
+        m = len(mat)
+        n = len(mat[0])
+
+        mp = {} # key/ i-j : value/[diagonal elements]
+
+        for i in range(m):
+            for j in range(n):
+                if i-j not in mp:
+                    mp[i-j] = []
+                mp[i-j].append(mat[i][j])
+
+        # now sort the values in map
+
+        for i in mp.values():
+            i.sort()
+
+        # place elements back in sorted fashion
+
+        for i in range(m-1, -1, -1): # reverse traverse
+            for j in range(n-1, -1, -1): 
+                mat[i][j] = mp[i-j][-1] # last element 
+                mp[i-j].pop()
+        return mat
