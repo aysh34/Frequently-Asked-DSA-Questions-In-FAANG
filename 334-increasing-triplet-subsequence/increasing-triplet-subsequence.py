f77b1e5896b2049cc38ae4b0f0aa8964fb63451c
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-01-03 00:00:18 +0500

    Update increasing-triplet-subsequence.py

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -6,12 +6,13 @@ class Solution:
         n1 = float('inf')
         n2 = float('inf')
 
-        for n3 in nums:
+       for n3 in nums:
+            # ju mjhe ab no mila hai agr ye n1 se chota ya equal hai tu n1 mei isko daal do
             if n3 <= n1:
                 n1 = n3
-            elif n3 <= n2:
+            elif n3 <= n2: # n3 agr ye n1 se chota nhi hai tu check kro kiya n2 se chota ya equal hai. agr hai tu n2 mei issy daal do
                 n2 = n3
-            else:
+            else: # agr na n1 se chota hai or na nhi n2 se chota hai tu mtlb ye condition n1 < n2 < n3 pora ho gya
                 return True
             
         return False
