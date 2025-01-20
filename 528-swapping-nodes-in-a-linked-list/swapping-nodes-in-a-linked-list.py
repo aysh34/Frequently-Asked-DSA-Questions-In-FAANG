0e3327c5f1a12c500fefd81fc8b870da48f66ba3
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-01-20 19:28:01 +0500

    Time: 63 ms (23.38%) | Memory: 40.2 MB (24.66%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,22 +5,40 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        l = 0
+        # 1 pass approach
         temp = head
+        p1 = None
+        p2 = None
+
         while temp:
-            l += 1
+            if p2 != None:
+                p2 = p2.next
+
+            k -= 1
+            if k == 0:
+                p1 = temp
+                p2 = head
             temp = temp.next
-
-        if k > l or k <= 0:
-            return head
-
-        node1 = head
-        for i in range(k - 1):
-            node1 = node1.next
-
-        node2 = head
-        for i in range(l - k):
-            node2 = node2.next
-
-        node1.val, node2.val = node2.val, node1.val
+       
+        p1.val, p2.val = p2.val, p1.val 
         return head
+
+        # l = 0
+        # temp = head
+        # while temp:
+        #     l += 1
+        #     temp = temp.next
+
+        # if k > l or k <= 0:
+        #     return head
+
+        # node1 = head
+        # for i in range(k - 1):
+        #     node1 = node1.next
+
+        # node2 = head
+        # for i in range(l - k):
+        #     node2 = node2.next
+
+        # node1.val, node2.val = node2.val, node1.val
+        # return head
