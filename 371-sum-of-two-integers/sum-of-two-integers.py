1c3f0de1c569fa1d0c4b57bf5fe5b363fb8a01d9
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-02-24 00:07:54 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.79%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,10 +1,15 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        MASK = 0xFFFFFFFF # 32-bit integer mask
+        MASK = 0xFFFFFFFF  # 32-bit integer mask
 
         while b != 0:
-            carry = (a & b) & MASK  # Carry in case of 1 1 
-            a = (a ^ b) & MASK # sum without carry
-            b =  (carry << 1) & MASK # left shift carry  
-        
+            carry = (a & b) & MASK  # Carry in case of 1 1
+            a = (a ^ b) & MASK  # sum without carry
+            b = (carry << 1) & MASK  # left shift carry
+
         return a if a <= 0x7FFFFFFF else ~(a ^ MASK)
+
+
+# a ^ MASK flips all bits of a : This converts the incorrect large positive number into a correct negative representation.
+
+# ~(...) negates it, converting it back to signed form.
