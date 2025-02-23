6148382b912803b691b4325240076da776b0e0b5
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-02-23 22:39:33 +0500

    Time: 5 ms (58.93%) | Memory: 18.6 MB (23.20%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,8 +1,19 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for i in range(n+1):
-            b = bin(i)
-            ans.append(b.count('1'))
+        res = [None] * (n + 1)
+        for i in range(n + 1):
+            if i == 0:
+                res[i] = 0
+            elif i % 2 == 0:  # even
+                res[i] = res[i // 2] # if i is 10 then it has same 1 bits as that of i//2 (i.e 5) has
+                
+            else:  # odd
+                res[i] = res[i // 2] + 1
+        return res
 
-        return ans
+        # ans = []
+        # for i in range(n+1):
+        #     b = bin(i)
+        #     ans.append(b.count('1'))
+
+        # return ans
