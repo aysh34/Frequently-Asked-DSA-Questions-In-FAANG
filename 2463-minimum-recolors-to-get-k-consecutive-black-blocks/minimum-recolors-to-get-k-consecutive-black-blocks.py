4a53e8344866dede8d5b20d368c7ca0512d3e224
Ayesha Saleem <134167523+aysh34@users.noreply.github.com>
2025-03-09 00:04:29 +0500

    Time: 3 ms (31.20%) | Memory: 17.9 MB (20.43%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,27 +1,36 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-#         n = len(blocks)
+        n = len(blocks)
 
-#         res = []
-#         for i in range(n - k + 1):
-#             res.append(blocks[i : i + k])
+        res = []
+        for i in range(n - k + 1):
+            res.append(blocks[i : i + k])
         
-#         min = float('inf')
-    # def minimumRecolors(blocks: str, k: int) -> int:
+        m = float('inf')
+        for i in res:
+            c = 0
+            for j in i:
+                if j == 'W':
+                    j = 'B'
+                    c += 1
+            m = min(m,c)
+
+        return m 
+
         # Count 'W' in the first window of size k
-        min_recolors = blocks[:k].count('W')
-        current_recolors = min_recolors
+        # min_recolors = blocks[:k].count('W')
+        # current_recolors = min_recolors
 
-        # Slide the window across the string
-        for i in range(k, len(blocks)):
-            # Remove the outgoing character (left side)
-            if blocks[i - k] == 'W':
-                current_recolors -= 1
-            # Add the incoming character (right side)
-            if blocks[i] == 'W':
-                current_recolors += 1
+        # # Slide the window across the string
+        # for i in range(k, len(blocks)):
+        #     # Remove the outgoing character (left side)
+        #     if blocks[i - k] == 'W':
+        #         current_recolors -= 1
+        #     # Add the incoming character (right side)
+        #     if blocks[i] == 'W':
+        #         current_recolors += 1
 
-            # Update minimum recolors required
-            min_recolors = min(min_recolors, current_recolors)
+        #     # Update minimum recolors required
+        #     min_recolors = min(min_recolors, current_recolors)
 
-        return min_recolors
+        # return min_recolors
